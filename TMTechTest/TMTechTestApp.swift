bfbd1ba484167bf24b7893fa3d688c0d17f327bd
Ants Gardiner <antokne@gmail.com>
2023-02-01 12:49:59 +1300

    Initial Commit

---
+++ TMTechTest/TMTechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMTechTestApp.swift
+//  TMTechTest
+//
+//  Created by Antony Gardiner on 1/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TMTechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
